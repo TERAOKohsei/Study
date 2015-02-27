34eb9096a28bd24663bb01587debc03e1e243b50
TERAO Kohsei <tera@chuo.co.jp>
2015-02-27 18:34:13 +0900

    測定部検討中

--- AsyncAwait/AsyncAwait/MainWindow.xaml.cs
+++ AsyncAwait/AsyncAwait/MainWindow.xaml.cs
@@ -22,10 +22,30 @@ namespace AsyncAwait {
             InitializeComponent();
         }
 
+        private Action<IMeasuringUnit, IResult> MeasureProcess2 = null;
+
+        private void MeasurementProcess() {
+            IMeasuringUnit ev = new Ev();
+            IMeasuringUnit eh = new Eh();
+            IResult pos = new Positioning();
+            IResult str = new Straightness();
+
+            MeasureProcess2 = new Action<IMeasuringUnit, IResult>((mu, r) => {
+                double[] mv;
+                mu.Measure(out mv);
+                r.Add(mv);
+            });
+
+            if ( MeasureProcess2 != null ) {
+                Parallel.Invoke(new []{MeasureProcess2(ev, pos)});
+            }
+        }
+
 
         private async void button1_Click(object sender, RoutedEventArgs e) {
             System.Diagnostics.Debug.WriteLine("Enter button1_Click");
-            await Task.Run(() => System.Threading.Thread.Sleep(10000));
+            //await Task.Run(() => System.Threading.Thread.Sleep(10000));
+            await Task.Run(() => MeasurementProcess());
             System.Diagnostics.Debug.WriteLine("Leave button1_Click");
         }
 
@@ -34,5 +54,74 @@ namespace AsyncAwait {
             await Task.Run(() => System.Threading.Thread.Sleep(5000));
             System.Diagnostics.Debug.WriteLine("Leave button2_Click");
         }
+    }
+
+    public interface IMeasuringUnit {
+        void Measure(out double[] values);
+    }
+
+    public interface IResult {
+        void Add(double[] values);
+        void Clear();
+    }
+
+    public class Ev : IMeasuringUnit {
+        #region IMeasuringUnit メンバー
+
+        public void Measure(out double[] values) {
+            System.Diagnostics.Debug.WriteLine("EVが測定");
+            values = new [] { 0.0, 1.0, 2.0 };
+        }
+
+        #endregion
+    }
+
+    public class Eh : IMeasuringUnit {
+        #region IMeasuringUnit メンバー
+
+        public void Measure(out double[] values) {
+            System.Diagnostics.Debug.WriteLine("EHが測定");
+            values = new[] { 0.0 };
+        }
+
+        #endregion
+    }
+
+    public class Positioning : IResult {
+        private List<double> measuredValue = new List<double>();
+
+        #region IResult メンバー
+
+        public void Add(double[] values) {
+            System.Diagnostics.Debug.WriteLine("Positioningに保存");
+            foreach ( var v in values ) {
+                measuredValue.Add(v);
+            }
+        }
+
+        public void Clear() {
+            measuredValue.Clear();
+        }
+
+        #endregion
+    }
+
+    public class Straightness : IResult {
+        private List<double> measuredValue = new List<double>();
+
+        #region IResult メンバー
+
+        public void Add(double[] values) {
+            System.Diagnostics.Debug.WriteLine("Positioningに保存");
+            foreach ( var v in values ) {
+                measuredValue.Add(v);
+            }
+        }
+
+        public void Clear() {
+            measuredValue.Clear();
+        }
+
+        #endregion
     }
 }
